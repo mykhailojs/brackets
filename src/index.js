f71baac000142ae2fd8d19c42cdfdf167459100a
mykhailojs <mykhailo.2024.ffs@gmail.com>
2024-10-25 12:37:34 +0300

    feat: finish app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const stack = [];
+
+    const data = new Map(bracketsConfig);
+    const closeItem = new Set(data.values());
+    const openItem = new Set(data.keys());
+
+
+
+
+    for (let char of str) {
+
+        if (openItem.has(char)) {
+
+            if (data.get(char) === char && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+
+
+        } else if (closeItem.has(char)) {
+            if (stack.length === 0 || data.get(stack.pop()) !== char) return false;
+        }
+    }
+
+
+    return stack.length === 0;
 }
